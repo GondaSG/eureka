fa66c7977218b2e17663b8adbc4101da7d1aa9f2
GSILVA <gsilva_dev@outlook.es>
2022-08-01 12:23:07 -0500

    modificacion

--- src/test/java/com/bootcampbc26/ServiceDiscovery/ServiceDiscoveryApplicationTests.java
+++ src/test/java/com/bootcampbc26/ServiceDiscovery/ServiceDiscoveryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sistemabancario.gateway;
+package com.bootcampbc26.ServiceDiscovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class ServiceDiscoveryApplicationTests {
 
 	@Test
 	void contextLoads() {
